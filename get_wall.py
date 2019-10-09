cfa423114808764588a273564a7a6e33a564c525
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-10-10 01:42:20 +0300

    get_wall update

--- get_wall.py
+++ get_wall.py
@@ -1,42 +1,92 @@
-from bs4 import BeautifulSoup
-import requests
 import os
 import random
 import sys
+import argparse
+
+from bs4 import BeautifulSoup
+import requests
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+MODE_DOWN, MODE_RAND, MODE_ALL = ('download', 'random', 'all')
+MODE_CHOICES = (MODE_DOWN, MODE_RAND, MODE_ALL)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--search', type=str, default='landscape')
+parser.add_argument('-p', '--max-page', type=int, default=1)
+parser.add_argument('-m', '--mode', type=str, choices=MODE_CHOICES, default=MODE_ALL)
+
+
+ARG = parser.parse_args()
 
 
 BASE_URL = 'https://www.pexels.com/search'
-URL = f'{BASE_URL}/landscape/'
+URL = '{}/{search}/'.format(BASE_URL, **ARG.__dict__)
+IMG_ROOT_DIR = os.path.join(ROOT_DIR, 'img')
+IMG_DIR = os.path.join(IMG_ROOT_DIR, ARG.search)
 
 
-doc = requests.get(URL)
-soup = BeautifulSoup(doc.text, 'html.parser')
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
 
 
-for i in soup.find_all('article', class_='photo-item'):
-    img = i.find('img', class_='photo-item__img')
-    url = img['src'].split('?')[0]
-
-    fn = url.split('/')[-1]
-    fp = os.path.join('img', fn)
-    if not os.path.isfile(fp):
-        print(f'Downloading "{url}"...')
-        response = requests.get(url)
-
-        with open(fp, 'wb') as f:
-            print(f'Saving to "{fp}"...')
-            f.write(response.content)
+headers = {
+    'user-agent': USER_AGENT,
+}
 
 
-img_list = os.listdir('img')
-fn = random.choice(img_list)
-full_path = os.path.join(ROOT_DIR, 'img', fn)
+for d in (IMG_ROOT_DIR, IMG_DIR):
+    if not os.path.isdir(d):
+        os.mkdir(d)
 
 
-if sys.platform == 'darwin':
-    os.system('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "{}"\''.format(full_path))
-else:
-    print('Random image is not supported on your operating system "{}"'.format(sys.platform))
+def download():
+    for page in range(1, ARG.max_page + 1):
+        if page == 1:
+            page_url = URL
+        else:
+            page_url = f'{URL}?page={page}'
+        print(f'Processing "{page_url}"')
+
+        doc = requests.get(page_url, headers=headers)
+        soup = BeautifulSoup(doc.text, 'html.parser')
+
+        for i in soup.find_all('article', class_='photo-item'):
+            img = i.find('img', class_='photo-item__img')
+            url = img['src'].split('?')[0]
+
+            fn = url.split('/')[-1]
+            fp = os.path.join(IMG_DIR, fn)
+
+            if not os.path.isfile(fp):
+                print(f'Downloading "{url}"...')
+                response = requests.get(url)
+
+                with open(fp, 'wb') as f:
+                    print(f'Saving to "{fp}"...')
+                    f.write(response.content)
+
+
+def apply_random_wallpaper():
+    img_list = os.listdir(IMG_DIR)
+
+    if not img_list:
+        print(f'"{IMG_DIR}" is empty, skip...')
+        return
+
+    random_fn = random.choice()
+    random_fp = os.path.join(IMG_DIR, random_fn)
+
+    if sys.platform == 'darwin':
+        os.system('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "{}"\''.format(random_fp))
+    else:
+        print('Random image is not supported on your operating system "{}"'.format(sys.platform))
+
+
+if ARG.mode == MODE_ALL:
+    download()
+    apply_random_wallpaper()
+elif ARG.mode == MODE_DOWN:
+    download()
+elif ARG.mode == MODE_RAND:
+    apply_random_wallpaper()
